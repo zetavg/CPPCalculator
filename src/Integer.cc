8b37de9a562351bbdd7330118911aaf24b18fba4
zetavg <mail@zeta.vg>
2017-04-30 23:31:01 +0800

    refactor code

--- src/Integer.cc
+++ src/Integer.cc
@@ -6,6 +6,10 @@
 #include <vector>
 #include <algorithm>
 
+/*
+ * Constructors
+ */
+
 Integer::Integer() {
     set_value("null");
 }
@@ -13,6 +17,10 @@ Integer::Integer() {
 Integer::Integer(const char *number) {
     set_value(number);
 }
+
+/*
+ * Getter and setters
+ */
 
 Integer& Integer::set_value(const char *number) {
     sign = true;
@@ -46,7 +54,7 @@ std::string Integer::get_value() {
 
         if (!sign) str.push_back('-');
 
-        for (std::vector<integer_digit_t>::iterator it = value.end() - 1; it >= value.begin(); --it) {
+        for (integer_value_t::iterator it = value.end() - 1; it >= value.begin(); --it) {
             str.push_back(*it + 48);
         }
 
@@ -54,11 +62,77 @@ std::string Integer::get_value() {
     }
 }
 
-void Integer::arrange() {
-    while (value.size() > 1 && value.back() == 0) {
-        value.pop_back();
+/*
+ * Value calculation helpers
+ */
+
+integer_value_t add_value(integer_value_t &a, integer_value_t &b) {
+    integer_value_t result;
+    int a_size = a.size();
+    int b_size = b.size();
+    int max_size = std::max(a_size, b_size);
+
+
+    for (int i = 0, carry = 0; i < max_size || carry > 0; ++i) {
+        int digit = carry;
+        if (i < a_size) digit += a.at(i);
+        if (i < b_size) digit += b.at(i);
+        carry = digit / 10;
+        digit = digit % 10;
+        result.push_back(digit);
+    }
+
+    return result;
+}
+
+integer_value_t subtract_value(integer_value_t &a, integer_value_t &b) {
+    integer_value_t result;
+    int a_size = a.size();
+    int b_size = b.size();
+
+    for (int i = 0, carry = 0; i < a_size; ++i) {
+        int digit = a.at(i) + carry;
+        carry = 0;
+        if (i < b_size) digit -= b.at(i);
+        if (digit < 0) {
+            digit += 10;
+            carry = -1;
+        }
+        result.push_back(digit);
+    }
+
+    return result;
+}
+
+int compare_value(integer_value_t &a, integer_value_t &b) {
+    int a_size = a.size();
+    int b_size = b.size();
+
+    if (a_size > b_size) {
+        return 1;
+    } else if (b_size > a_size) {
+        return -1;
+    } else {
+        for (
+            integer_value_t::iterator a_i = a.end() - 1,
+                                      b_i = b.end() - 1;
+            a_i >= a.begin() && b_i >= b.begin();
+            --a_i, --b_i
+        ) {
+            if (*a_i > *b_i) {
+                return 1;
+            } else if (*b_i > *a_i) {
+                return -1;
+            }
+        }
+
+        return 0;
     }
 }
+
+/*
+ * Overload operators
+ */
 
 Integer& Integer::operator=(const char *number) {
     set_value(number);
@@ -78,121 +152,73 @@ std::istream& operator>>(std::istream &in, Integer &integer) {
     return in;
 }
 
-std::vector<integer_digit_t> add_value(std::vector<integer_digit_t> &a, std::vector<integer_digit_t> &b) {
-    std::vector<integer_digit_t> result;
-    int a_size = a.size();
-    int b_size = b.size();
-    int max_size = std::max(a_size, b_size);
-
-
-    for (int i = 0, carry = 0; i < max_size || carry > 0; ++i) {
-        int digit = carry;
-        if (i < a_size) digit += a.at(i);
-        if (i < b_size) digit += b.at(i);
-        carry = digit / 10;
-        digit = digit % 10;
-        result.push_back(digit);
-    }
-
-    return result;
-}
-
-std::vector<integer_digit_t> subtract_value(std::vector<integer_digit_t> &a, std::vector<integer_digit_t> &b) {
-    std::vector<integer_digit_t> result;
-    int a_size = a.size();
-    int b_size = b.size();
-
-    for (int i = 0, carry = 0; i < a_size; ++i) {
-        int digit = a.at(i) + carry;
-        carry = 0;
-        if (i < b_size) digit -= b.at(i);
-        if (digit < 0) {
-            digit += 10;
-            carry = -1;
-        }
-        result.push_back(digit);
-    }
-
-    return result;
-}
-
-int compare_value(std::vector<integer_digit_t> &a, std::vector<integer_digit_t> &b) {
-    int a_size = a.size();
-    int b_size = b.size();
-
-    if (a_size > b_size) {
-        return 1;
-    } else if (b_size > a_size) {
-        return -1;
-    } else {
-        for (std::vector<integer_digit_t>::iterator a_i = a.end() - 1, b_i = b.end() - 1; a_i >= a.begin() && b_i >= b.begin(); --a_i, --b_i) {
-            if (*a_i > *b_i) {
-                return 1;
-            } else if (*b_i > *a_i) {
-                return -1;
-            }
-        }
-
-        return 0;
-    }
-}
-
 Integer operator+(Integer &a, Integer &b) {
     if (a.sign && b.sign) {
+        // simply add the values if both a and b are both positive
         Integer result;
         result.sign = true;
         result.value = add_value(a.value, b.value);
         return result;
-    } else if (a.sign && !b.sign) {
-        int c = compare_value(a.value, b.value);
 
-        if (c > 0) {
-            Integer result;
-            result.sign = true;
-            result.value = subtract_value(a.value, b.value);
-            result.arrange();
-            return result;
-        } else if (c < 0) {
-            Integer result;
-            result.sign = false;
-            result.value = subtract_value(b.value, a.value);
-            result.arrange();
-            return result;
-        } else {
-            Integer result("0");
-            return result;
-        }
-    } else if (!a.sign && b.sign) {
-        int c = compare_value(a.value, b.value);
-
-        if (c > 0) {
-            Integer result;
-            result.sign = false;
-            result.value = subtract_value(a.value, b.value);
-            result.arrange();
-            return result;
-        } else if (c < 0) {
-            Integer result;
-            result.sign = true;
-            result.value = subtract_value(b.value, a.value);
-            result.arrange();
-            return result;
-        } else {
-            Integer result("0");
-            return result;
-        }
     } else if (!a.sign && !b.sign) {
+        // add the values and set the sign to negative if both a and b are both negative
         Integer result;
         result.sign = false;
         result.value = add_value(a.value, b.value);
         return result;
+
+    } else {
+        // we need to compare the values and perform an substraction
+        Integer *positive_integer, *negative_integer;
+
+        // find the positive and negative integer
+        if (a.sign && !b.sign) {
+            positive_integer = &a;
+            negative_integer = &b;
+        } else {
+            positive_integer = &b;
+            negative_integer = &a;
+        }
+
+        // compare the values to decide what to do
+        int comparing_result = compare_value(positive_integer->value, negative_integer->value);
+
+        if (comparing_result > 0) {
+            // the positive value is larger then the negative value
+            Integer result;
+            result.sign = true;
+            result.value = subtract_value(positive_integer->value, negative_integer->value);
+            result.arrange();
+            return result;
+        } else if (comparing_result < 0) {
+            // the negative value is larger then the positive value
+            Integer result;
+            result.sign = false;
+            result.value = subtract_value(negative_integer->value, positive_integer->value);
+            result.arrange();
+            return result;
+        } else {
+            // the values are the same
+            Integer result("0");
+            return result;
+        }
     }
 }
 
-Integer operator-(Integer &a, Integer &b) {
-    return Integer();
-}
+// Integer operator-(Integer &a, Integer &b) {
+//     return Integer();
+// }
 
-Integer operator*(Integer &a, Integer &b) {
-    return Integer();
+// Integer operator*(Integer &a, Integer &b) {
+//     return Integer();
+// }
+
+/*
+ * Private methods
+ */
+
+void Integer::arrange() {
+    while (value.size() > 1 && value.back() == 0) {
+        value.pop_back();
+    }
 }
--- src/Integer.h
+++ src/Integer.h
@@ -6,13 +6,17 @@
 #include <vector>
 
 typedef char integer_digit_t;
+typedef std::vector<integer_digit_t> integer_value_t;
 
 class Integer {
  public:
+    /* Constructors */
     Integer();
     Integer(const char*);
+    /* Getter and setters */
     Integer& set_value(const char*);
     std::string get_value();
+    /* Overload operators */
     Integer& operator=(const char*);
     friend std::ostream& operator<<(std::ostream &out, Integer &integer);
     friend std::istream& operator>>(std::istream &in, Integer &integer);
@@ -23,9 +27,11 @@ class Integer {
     friend bool operator>(Integer &a, Integer &b);
     // friend Integer operator/(Integer &a, Integer &b);
  private:
-    std::vector<integer_digit_t> value;
+    /* Private members */
+    integer_value_t value;
     bool sign;
-    void arrange();
+    /* Private methods */
+    void arrange(); // Remove leading zeros from the value
 };
 
 #endif
